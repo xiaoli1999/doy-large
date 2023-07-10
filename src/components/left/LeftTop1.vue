90a2e4527b3263ce90aea600ac9fb25fa3ab83d7
小黎 <22708206@qq.com>
2023-07-10 17:24:21 +0800

    feat(feat): 新增图表1
    新增图表

--- src/components/left/LeftTop1.vue
+++ src/components/left/LeftTop1.vue
@@ -7,255 +7,300 @@
 
 <script>
 import * as echarts from 'echarts'
+
 export default {
-    name: 'LeftTop1',
-    data () {
-        return {
-            color: ['#EBC53F', '#3D97EE'],
-            list: [
-                { name: 'xx站', value: 160 },
-                { name: 'xx站', value: 180 },
-                { name: 'xx站', value: 150 },
-                { name: 'xx站', value: 140 },
-                { name: 'xx站', value: 170 }
-            ]
-        }
-    },
-    mounted () {
-        this.initChart()
-    },
-    methods: {
-        initChart () {
-            const chartDom = document.getElementById('chart-1')
-            const myChart = echarts.init(chartDom)
-            myChart.clear()
+	name: 'LeftTop1',
+	data () {
+		return {
+			color: ['#EBC53F', '#3D97EE'],
+			list: [
+				{ name: 'xx站', value: 160 },
+				{ name: 'xx站', value: 180 },
+				{ name: 'xx站', value: 150 },
+				{ name: 'xx站', value: 140 },
+				{ name: 'xx站', value: 170 }
+			]
+		}
+	},
+	mounted () {
+		this.initChart()
+	},
+	methods: {
+		initChart () {
+			const chartDom = document.getElementById('chart-1')
+			const myChart = echarts.init(chartDom)
+			myChart.clear()
 
-            const offsetX = 8
-            const offsetY = 4
-            // 绘制左侧面
-            const CubeLeft = echarts.graphic.extendShape({
-                shape: {
-                    x: 0,
-                    y: 0
-                },
-                buildPath: function (ctx, shape) {
-                    // 会canvas的应该都能看得懂，shape是从custom传入的
-                    const xAxisPoint = shape.xAxisPoint
-                    // console.log(shape);
-                    const c0 = [shape.x, shape.y]
-                    const c1 = [shape.x - offsetX, shape.y - offsetY]
-                    const c2 = [xAxisPoint[0] - offsetX, xAxisPoint[1] - offsetY]
-                    const c3 = [xAxisPoint[0], xAxisPoint[1]]
-                    ctx.moveTo(c0[0], c0[1]).lineTo(c1[0], c1[1]).lineTo(c2[0], c2[1]).lineTo(c3[0], c3[1]).closePath()
-                }
-            })
-            // 绘制右侧面
-            const CubeRight = echarts.graphic.extendShape({
-                shape: {
-                    x: 0,
-                    y: 0
-                },
-                buildPath: function (ctx, shape) {
-                    const xAxisPoint = shape.xAxisPoint
-                    const c1 = [shape.x, shape.y]
-                    const c2 = [xAxisPoint[0], xAxisPoint[1]]
-                    const c3 = [xAxisPoint[0] + offsetX, xAxisPoint[1] - offsetY]
-                    const c4 = [shape.x + offsetX, shape.y - offsetY]
-                    ctx.moveTo(c1[0], c1[1]).lineTo(c2[0], c2[1]).lineTo(c3[0], c3[1]).lineTo(c4[0], c4[1]).closePath()
-                }
-            })
-            // 绘制顶面
-            const CubeTop = echarts.graphic.extendShape({
-                shape: {
-                    x: 0,
-                    y: 0
-                },
-                buildPath: function (ctx, shape) {
-                    const c1 = [shape.x, shape.y]
-                    const c2 = [shape.x + offsetX, shape.y - offsetY] // 右点
-                    const c3 = [shape.x, shape.y - offsetX]
-                    const c4 = [shape.x - offsetX, shape.y - offsetY]
-                    ctx.moveTo(c1[0], c1[1]).lineTo(c2[0], c2[1]).lineTo(c3[0], c3[1]).lineTo(c4[0], c4[1]).closePath()
-                }
-            })
-            // 注册三个面图形
-            echarts.graphic.registerShape('CubeLeft', CubeLeft)
-            echarts.graphic.registerShape('CubeRight', CubeRight)
-            echarts.graphic.registerShape('CubeTop', CubeTop)
+			const data = [140, 160, 180, 150, 168]
+			const colorArr = ['#59B1FF', '#59B1FF', '#59B1FF', '#59B1FF', '#59B1FF']
+			const colorArr1 = ['#3688FF', '#3688FF', '#3688FF', '#3688FF', '#3688FF']
+			const color = {
+				type: 'linear',
+				x: 0,
+				x2: 1,
+				y: 0,
+				y2: 0,
+				colorStops: [
+					{
+						offset: 0,
+						color: colorArr[0]
+					},
+					{
+						offset: 0.5,
+						color: colorArr[0]
+					},
+					{
+						offset: 0.5,
+						color: colorArr[1]
+					},
+					{
+						offset: 1,
+						color: colorArr[1]
+					},
+					{
+						offset: 1,
+						color: colorArr[1]
+					}
+				]
+			}
+			const color1 = {
+				type: 'linear',
+				x: 0,
+				x2: 1,
+				y: 0,
+				y2: 0,
+				colorStops: [
+					{
+						offset: 0,
+						color: colorArr1[0]
+					},
+					{
+						offset: 0.5,
+						color: colorArr1[0]
+					},
+					{
+						offset: 0.5,
+						color: colorArr1[1]
+					},
+					{
+						offset: 1,
+						color: colorArr1[1]
+					},
+					{
+						offset: 1,
+						color: colorArr1[1]
+					}
+				]
+			}
+			const dataColor = [
+				500,
+				800,
+				900,
+				680,
+				{
+					value: 600,
+					itemStyle: {
+						color: color1
+					}
+				}
+			]
+			const data2 = [
+				500,
+				800,
+				900,
+				680,
+				{
+					value: 600,
+					itemStyle: {
+						color: colorArr1[2]
+					}
+				}
+			]
+			// const sideData = dataColor.map(item => {
+			// 	if (typeof item === 'number') {
+			// 		return item
+			// 	} else {
+			// 		console.log(colorArr1[2])
+			// 		return {
+			// 			value: item,
+			// 			itemStyle: {
+			// 				color: colorArr1[2]
+			// 			}
+			// 		}
+			// 	}
+			// })
 
-            const option = {
-                backgroundColor: 'rgba(0,0,0, .6)',
-                legend: {
-                    itemHeight: 12, itemWidth: 12,
-                    right: '0%',
-                    top: '8%',
-                    textStyle: { fontSize: '12', color: '#fff' }
-                },
-                color: this.color,
-                tooltip: {
-                    trigger: 'axis',
-                    axisPointer: {
-                        type: 'shadow'
-                    },
-                    formatter: function (params) {
-                        const item = params[1]
-                        return item.name + ' : ' + item.value
-                    }
-                },
-                grid: {
-                    left: '5%',
-                    right: '5%',
-                    top: '20%',
-                    bottom: '5%',
-                    containLabel: true
-                },
-                xAxis: {
-                    type: 'category',
-                    data: this.list.map(i => i.name),
-                    axisLine: {
-                        show: true,
-                        lineStyle: {
-                            color: '#f4f4f4'
-                        }
-                    },
-                    textStyle: {
-                        color: '#fff'
-                    },
-                    axisTick: {
-                        show: false
-                    },
-                    axisLabel: {
-                        fontSize: 10
-                    }
-                },
-                yAxis: {
-                    type: 'value',
-                    axisTick: {
-                        show: false
-                    },
-                    splitLine: {
-                        show: false
-                    },
-                    axisLabel: {
-                        fontSize: 12,
-                        show: false
-                    }
-                },
-                series: [
-                    {
-                        type: 'custom',
-                        renderItem: (params, api) => {
-                            const location = api.coord([api.value(0), api.value(1)])
-                            return {
-                                type: 'group',
-                                children: [
-                                    {
-                                        type: 'CubeLeft',
-                                        shape: {
-                                            api,
-                                            xValue: api.value(0),
-                                            yValue: api.value(1),
-                                            x: location[0],
-                                            y: location[1],
-                                            xAxisPoint: api.coord([api.value(0), 0])
-                                        },
-                                        style: {
-                                            fill: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                                                {
-                                                    offset: 0,
-                                                    color: '#59B1FF'
-                                                },
-                                                {
-                                                    offset: 1,
-                                                    color: '#3688FF'
-                                                }
-                                            ])
-                                        }
-                                    },
-                                    {
-                                        type: 'CubeRight',
-                                        shape: {
-                                            api,
-                                            xValue: api.value(0),
-                                            yValue: api.value(1),
-                                            x: location[0],
-                                            y: location[1],
-                                            xAxisPoint: api.coord([api.value(0), 0])
-                                        },
-                                        style: {
-                                            fill: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                                                {
-                                                    offset: 0,
-                                                    color: '#59B1FF'
-                                                },
-                                                {
-                                                    offset: 1,
-                                                    color: '#3688FF'
-                                                }
-                                            ])
-                                        }
-                                    },
-                                    {
-                                        type: 'CubeTop',
-                                        shape: {
-                                            api,
-                                            xValue: api.value(0),
-                                            yValue: api.value(1),
-                                            x: location[0],
-                                            y: location[1],
-                                            xAxisPoint: api.coord([api.value(0), 0])
-                                        },
-                                        style: {
-                                            fill: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                                                {
-                                                    offset: 0,
-                                                    color: '#62ADFF'
-                                                },
-                                                {
-                                                    offset: 1,
-                                                    color: '#62ADFF'
-                                                }
-                                            ])
-                                        }
-                                    }
-                                ]
-                            }
-                        },
-                        data: this.list.map(i => i.value)
-                    },
-                    {
-                        type: 'bar',
-                        itemStyle: {
-                            color: 'transparent'
-                        },
-                        tooltip: {},
-                        data: this.list.map(i => i.value)
-                    },
-                    {
-                        name: '',
-                        barGap: '-100%',
-                        type: 'bar',
-                        z: 0,
-                        data: [240, 240, 240, 240, 240],
-                        barWidth: '60%',
-                        itemStyle: {
-                            normal: {
-                                color: '#113D6B60' // 0% 处的颜色
-                            }
-                        }
+			const barWidth = 20
+			const option = {
+				tooltip: {
+					trigger: 'axis',
+					formatter: function (params) {
+						var str = params[0].name + ':'
+						params.filter(function (item) {
+							if (item.componentSubType == 'bar') {
+								str += '<br/>' + item.seriesName + '：' + item.value
+							}
+						})
+						return str
+					}
+				},
+				grid: {
+					top: '18%',
+					left: '10%',
+					right: '10%',
+					bottom: '18%'
+				},
+				legend: {
+					show: true,
+					icon: 'rect',
+					itemHeight: 12,
+					itemWidth: 12,
+					fontSize: 14,
+					data: [
+						{
+							name: '室内接发车',
+							icon: 'rect',
+							// 设置文本为红色
+							itemStyle: {
+								color: '#3887D5'
+							}
+						},
+						{
+							name: '室内接发车1',
+							icon: 'rect',
+							// 设置文本为红色
+							itemStyle: {
+								color: '#cfd181'
+							}
+						}
+					],
+					textStyle: {
+						color: '#fff',
+						fontSize: '16'
+					}
+				},
+				xAxis: {
+					data: ['xx站', 'xx站', 'xx站', 'xx站', 'xx站'],
+					// show: false,
+					// 坐标轴
+					axisLine: {
+						onZero: false,
+						lineStyle: {
+							width: 1,
+							color: '#214776'
+						}
+					},
+					spliteLine: {
+						show: false
+					},
+					offset: 5,
+					type: 'category',
+					axisLabel: {
+						textStyle: {
+							color: '#C5DFFB',
+							fontWeight: 500,
+							fontSize: '14'
+						}
+					},
+					axisTick: {
+						show: true
+					}
+				},
+				yAxis: {
+					type: 'value',
+					// 坐标轴
+					axisLine: {
+						show: false
+					},
+					axisTick: {
+						show: false
+					},
+					// 坐标值标注
+					axisLabel: {
+						show: false
+					},
+					// 分格线
+					splitLine: {
+						show: false,
+						lineStyle: {
+							color: '#13365f'
+						}
+					}
+				},
+				series: [
+					{
+						z: 1,
+						name: '室内接发车',
+						type: 'bar',
+						barWidth: barWidth,
+						barGap: '50%',
+						data: dataColor,
+						showBackground: true,
+						label: {
+							normal: {
+								show: true, // 开启显示
+								position: 'top', // 柱形上方
+								textStyle: {
+									// 数值样式
+									color: '#fff'
+								}
+							}
+						},
+						itemStyle: {
+							normal: {
+								color: color
+							}
+						}
+					},
+					{
+						// 底
+						z: 2,
+						name: '室内接发车1',
+						type: 'pictorialBar',
+						data: dataColor,
+						symbol: 'diamond',
+						symbolOffset: ['0%', '50%'],
+						symbolSize: [barWidth, 8],
+						itemStyle: {
+							normal: {
+								color: color
+							}
+						},
+						tooltip: {
+							show: false
+						}
+					},
+					{
+						// 顶
+						z: 3,
+						name: '室内接发车',
+						type: 'pictorialBar',
+						symbolPosition: 'end',
+						data: data2,
+						symbol: 'diamond',
+						symbolOffset: ['0%', '-50%'],
+						symbolSize: [barWidth, 8],
+						itemStyle: {
+							normal: {
+								borderWidth: 2,
+								color: colorArr[2]
+							}
+						},
+						tooltip: {
+							show: false
+						}
+					}
+				]
+			}
 
-                    }
-                ]
-            }
+			myChart.setOption(option)
+		},
 
-            myChart.setOption(option)
-        },
-
-        changeChart (i) {
-            this.cateIndex = i
-            this.initChart()
-        }
-    }
+		changeChart (i) {
+			this.cateIndex = i
+			this.initChart()
+		}
+	}
 }
 </script>
 
